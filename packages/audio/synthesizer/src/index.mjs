f76c608ee8a173d0f9cc3d6c309c8e8756fe7f83
m-nakasato <70959675+m-nakasato@users.noreply.github.com>
2026-03-16 09:58:25 +0900

    refactor: reorganize audio package directory structure (#6)

---
+++ packages/audio/synthesizer/src/index.mjs
@@ -0,0 +1,6 @@
+export { Synthesizer } from './synthesizer/Synthesizer.mjs';
+export { Wave } from './synthesizer/Wave.mjs';
+export { presetWaveStrategy } from './synthesizer/waveStrategies/presetWaveStrategy.mjs';
+export { tableWaveStrategy } from './synthesizer/waveStrategies/tableWaveStrategy.mjs';
+export { noiseWaveStrategy } from './synthesizer/waveStrategies/noiseWaveStrategy.mjs';
+export { lfsr } from './synthesizer/utils/lfsr.mjs';
