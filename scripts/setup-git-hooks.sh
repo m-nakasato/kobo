3b474528e847e7dbabb7654d6c25bd91de290a9c
m-nakasato <70959675+m-nakasato@users.noreply.github.com>
2026-05-19 09:56:44 +0900

    Add message to pre-push hook

--- scripts/setup-git-hooks.sh
+++ scripts/setup-git-hooks.sh
@@ -2,5 +2,5 @@ git config hook.runHook true
 git config hook.pre-commit.event pre-commit
 git config hook.pre-commit.command "npm run lint"
 git config hook.pre-push.event pre-push
-git config hook.pre-push.command "sh -c 'npm test'"
+git config hook.pre-push.command "sh -c 'npm test || { echo \"Test failed. Push aborted.\" >&2; exit 1; }'"
 
