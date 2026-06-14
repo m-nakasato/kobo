d00a6f5a0971e4aa2d8052055a93fd7611695d02
m-nakasato <70959675+m-nakasato@users.noreply.github.com>
2026-06-14 11:41:38 +0900

    refactor params and add validator

--- packages/audio/synthesizer/src/Synthesizer.mjs
+++ packages/audio/synthesizer/src/Synthesizer.mjs
@@ -14,41 +14,82 @@ export class Synthesizer {
         let gainNode = new GainNode(this.#audioCtx, { 'gain': depth });
         lfo.connect(gainNode).connect(target);
     }
-    static envelope(gain, sTime, eTime, vol, adsr = []) {
-        let [a = 0.01, d = 0.01, s = 0.5, r = 0.01] = adsr;
-        gain.setValueAtTime(0, sTime);
-        gain.linearRampToValueAtTime(vol, sTime + a);
-        gain.linearRampToValueAtTime(vol * s, sTime + a + d);
-        gain.setValueAtTime(vol * s, eTime - r);
-        gain.linearRampToValueAtTime(0, eTime);
+    static envelope(gain, startTime, endTime, volume, envelope) {
+        let [a = 0.01, d = 0.01, s = 0.5, r = 0.01] = envelope;
+        gain.setValueAtTime(0, startTime);
+        gain.linearRampToValueAtTime(volume, startTime + a);
+        gain.linearRampToValueAtTime(volume * s, startTime + a + d);
+        gain.setValueAtTime(volume * s, endTime - r);
+        gain.linearRampToValueAtTime(0, endTime);
     }
     play(
         noteNumber,
         {
-            'mode': mode = 0,
-            'sTime': sTime = this.#audioCtx.currentTime,
-            'dur': dur = 0.03,
-            'vol': vol = 1,
-            'det': det = 0,
-            'swp': swp = 0,
-            'env': env,
-            'vib': vib,
-            'trm': trm,
+            'mod': mode = 0,
+            'sta': startTime = this.#audioCtx.currentTime,
+            'dur': duration = 0.03,
+            'vol': volume = 1,
+            'det': detune = 0,
+            'swp': sweep = 0,
+            'env': envelope = [],
+            'vib': vibration,
+            'trm': tremolo,
         } = {},
     ) {
-        let eTime = sTime + dur;
+        if (__DEV__) {
+            if (Number.isInteger(mode) == false || mode < 0 || mode >= this.#waves.length)
+                throw new Error('Invalid mode');
+            if (Number.isNaN(startTime) || startTime < 0) throw new Error('Invalid start time');
+            if (Number.isNaN(duration) || duration <= 0) throw new Error('Invalid duration');
+            if (Number.isNaN(volume) || volume < 0 || volume > 2) throw new Error('Invalid volume');
+            if (Number.isNaN(detune) || detune < -3600 || detune > 3600)
+                throw new Error('Invalid detune');
+            if (Number.isNaN(sweep) || sweep < -3600 || sweep > 3600)
+                throw new Error('Invalid sweep');
+            if (Array.isArray(envelope) == false || envelope.length > 4)
+                throw new Error('Invalid envelope');
+            if (envelope[0] + envelope[1] + envelope[3] > duration)
+                throw new Error('Invalid envelope');
+            if (envelope[2] < 0 || envelope[2] > 1)
+                throw new Error('Invalid envelope sustain level');
+            envelope.forEach(value => {
+                if (Number.isNaN(value) || value < 0) throw new Error('Invalid envelope value');
+            });
+            if (vibration != undefined) {
+                if (Array.isArray(vibration) == false || vibration.length > 3)
+                    throw new Error('Invalid vibration');
+                let [depth, rate, wave] = vibration;
+                if (Number.isNaN(depth) || depth < 0 || depth > 100)
+                    throw new Error('Invalid vibration depth');
+                if (Number.isNaN(rate) || rate <= 0 || rate > 10)
+                    throw new Error('Invalid vibration rate');
+                if (typeof wave != 'string') throw new Error('Invalid vibration wave');
+            }
+            if (tremolo != undefined) {
+                if (Array.isArray(tremolo) == false || tremolo.length > 3)
+                    throw new Error('Invalid tremolo');
+                let [depth, rate, wave] = tremolo;
+                if (Number.isNaN(depth) || depth < 0 || depth > 1)
+                    throw new Error('Invalid tremolo depth');
+                if (Number.isNaN(rate) || rate <= 0 || rate > 10)
+                    throw new Error('Invalid tremolo rate');
+                if (typeof wave != 'string') throw new Error('Invalid tremolo wave');
+            }
+        }
+        let endTime = startTime + duration;
         let src = this.#waves[mode].getSourceNode(noteNumber);
-        src.detune.value = det;
-        src.detune.linearRampToValueAtTime(det + swp, eTime);
-        if (src.frequency != undefined && vib != undefined) this.#lfo(src.frequency, ...vib);
-        // if (!(this.#wav instanceof TableWave)) vol /= 4;
+        src.detune.value = detune;
+        src.detune.linearRampToValueAtTime(detune + sweep, endTime);
+        if (src.frequency != undefined && vibration != undefined)
+            this.#lfo(src.frequency, ...vibration);
+        // if (!(this.#wav instanceof TableWave)) volume /= 4;
         let gainNode = new GainNode(this.#audioCtx);
-        Synthesizer.envelope(gainNode.gain, sTime, eTime, vol, env);
-        if (trm != undefined) this.#lfo(gainNode.gain, ...trm);
+        Synthesizer.envelope(gainNode.gain, startTime, endTime, volume, envelope);
+        if (tremolo != undefined) this.#lfo(gainNode.gain, ...tremolo);
         if (this.#analyserNode) gainNode.connect(this.#analyserNode);
         src.connect(gainNode).connect(this.#audioCtx.destination);
-        src.start(sTime);
-        src.stop(eTime);
+        src.start(startTime);
+        src.stop(endTime);
         let UUID = crypto.randomUUID();
         this.#tasks[UUID] = src;
         src.onended = () => {
@@ -60,7 +101,7 @@ export class Synthesizer {
             delete this.#tasks[UUID];
         };
         // return {eTime, freq: this.#wav.freq(pitch)};
-        return { eTime };
+        return { end: endTime };
     }
     discard() {
         Object.keys(this.#tasks).forEach(key => this.#tasks[key].stop());
