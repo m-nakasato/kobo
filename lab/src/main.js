c3d96c9aaf00fadd4035df7c632d4450a1d8c575
m-nakasato <70959675+m-nakasato@users.noreply.github.com>
2026-06-19 12:49:13 +0900

    Separate validator

--- lab/src/main.js
+++ lab/src/main.js
@@ -12,82 +12,98 @@ const audioCtx = new AudioContext();
 
 console.log('Sample Rate:', audioCtx.sampleRate);
 
-document.getElementById('playPreset').onclick = () => {
-    const waves = [
-        new Wave(audioCtx, presetWaveStrategy, 'sine'),
-        new Wave(audioCtx, presetWaveStrategy, 'square'),
-        new Wave(audioCtx, presetWaveStrategy, 'sawtooth'),
-        new Wave(audioCtx, presetWaveStrategy, 'triangle'),
-    ];
+const presetWaves = [
+    new Wave(audioCtx, presetWaveStrategy, 'sine'),
+    new Wave(audioCtx, presetWaveStrategy, 'square'),
+    new Wave(audioCtx, presetWaveStrategy, 'sawtooth'),
+    new Wave(audioCtx, presetWaveStrategy, 'triangle'),
+];
 
-    const synthesizer = new Synthesizer(audioCtx, waves);
-    // synthesizer.play(null);
-    // synthesizer.play(69, { 'dur': 0.25, 'mode': 2, 'swp': 300 });
-    synthesizer.play(60, { dur: 0.5, mode: 2, vib: [20, 5, 'sawtooth'] });
-    // synthesizer.play('A4', { dur: 1, mode: 2, trm: [0.15, 5, 'sine'] });
-    // synthesizer.play('A4', { dur: 1, mode: 2 });
+const tableWaves = [
+    new Wave(audioCtx, tableWaveStrategy, 'F0'),
+    new Wave(audioCtx, tableWaveStrategy, 'F000'),
+    new Wave(audioCtx, tableWaveStrategy, 'F0000000'),
+    new Wave(audioCtx, tableWaveStrategy, '0123456789ABCDEFFEDCBA9876543210'),
+];
+
+// let periods = [
+//     ...new Set(
+//         [...Array(15)].flatMap((_, i) => Math.round(audioCtx.sampleRate / 441 / 1.5 ** i)),
+//     ),
+// ].filter(Boolean);
+const FC_PERIODS = [4068, 2034, 1016, 762, 508, 380, 254, 202, 160, 128, 96, 64, 32, 16, 8, 4];
+let periods = [
+    ...new Set(FC_PERIODS.map(fcp => Math.round((audioCtx.sampleRate / 1789772.5) * fcp))),
+].filter(Boolean);
+console.log(periods);
+const noiseWaves = [
+    new Wave(audioCtx, noiseWaveStrategy),
+    new Wave(audioCtx, noiseWaveStrategy, periods),
+    new Wave(audioCtx, noiseWaveStrategy, periods, lfsr()),
+    new Wave(audioCtx, noiseWaveStrategy, periods, lfsr(6)),
+];
+
+const synthesizers = [
+    new Synthesizer(audioCtx, presetWaves),
+    new Synthesizer(audioCtx, tableWaves),
+    new Synthesizer(audioCtx, noiseWaves),
+];
+
+document.querySelector('#play').onclick = () => {
+    const noteNumber = parseInt(
+        document.querySelector('#demo input[name=pitch]:checked').value,
+        10,
+    );
+    const wave = document.querySelector('#demo input[name=wave]:checked').value;
+    let mode;
+    if (wave === '0') {
+        mode = document.querySelector('#demo select[name=presetMode]').value;
+    } else if (wave === '1') {
+        mode = document.querySelector('#demo select[name=tableMode]').value;
+    } else if (wave === '2') {
+        mode = document.querySelector('#demo select[name=noiseMode]').value;
+    }
+    const startTime = document.querySelector('#demo input[name=startTime]').value;
+    const duration = document.querySelector('#demo input[name=duration]').valueAsNumber;
+    const volume = document.querySelector('#demo input[name=volume]').valueAsNumber;
+    const detune = document.querySelector('#demo input[name=detune]').value;
+    const sweep = document.querySelector('#demo input[name=sweep]').valueAsNumber;
+    const envAttack = document.querySelector('#demo input[name=envAttack]').valueAsNumber;
+    const envDecay = document.querySelector('#demo input[name=envDecay]').valueAsNumber;
+    const envSustain = document.querySelector('#demo input[name=envSustain]').valueAsNumber;
+    const envRelease = document.querySelector('#demo input[name=envRelease]').valueAsNumber;
+    const envelope = [envAttack, envDecay, envSustain, envRelease];
+    const vibratoDepth = document.querySelector('#demo input[name=vibDepth]').valueAsNumber;
+    const vibratoRate = document.querySelector('#demo input[name=vibRate]').valueAsNumber;
+    const vibratoWave = document.querySelector(
+        '#demo select[name=vibWave] option:checked',
+    ).innerText;
+    const tremoloDepth = document.querySelector('#demo input[name=trmDepth]').valueAsNumber;
+    const tremoloRate = document.querySelector('#demo input[name=trmRate]').valueAsNumber;
+    const tremoloWave = document.querySelector(
+        '#demo select[name=trmWave] option:checked',
+    ).innerText;
+    const options = {};
+    if (startTime !== '') options.sta = parseFloat(startTime);
+    if (mode !== '') options.mod = parseInt(mode, 10);
+    options.dur = duration;
+    options.vol = volume;
+    if (detune !== '') options.det = parseFloat(detune);
+    if (sweep !== 0) {
+        options.swp = sweep;
+    }
+    if (envAttack != 0.01 || envDecay != 0.01 || envSustain != 0.5 || envRelease != 0.01) {
+        options.env = envelope;
+    }
+    if (wave != 2 && vibratoDepth > 0 && vibratoRate > 0) {
+        options.vib = [vibratoDepth, vibratoRate, vibratoWave];
+    }
+    if (tremoloDepth > 0 && tremoloRate > 0) {
+        options.trm = [tremoloDepth, tremoloRate, tremoloWave];
+    }
+    console.log(options);
+    synthesizers[wave].play(noteNumber, options);
 };
-
-document.getElementById('playTable').onclick = () => {
-    const waves = [
-        new Wave(audioCtx, tableWaveStrategy, 'F0'),
-        new Wave(audioCtx, tableWaveStrategy, 'F000'),
-        new Wave(audioCtx, tableWaveStrategy, 'F0000000'),
-        new Wave(audioCtx, tableWaveStrategy, '0123456789ABCDEFFEDCBA9876543210'),
-    ];
-
-    const synthesizer = new Synthesizer(audioCtx, waves);
-    // synthesizer.play(null);
-    synthesizer.play(69, { 'dur': 0.25, 'mode': 0, 'swp': 300 });
-    // synthesizer.play('A4', { dur: 1, mode: 2, vib: [20, 5, 'sawtooth'] });
-    // synthesizer.play('A4', { dur: 1, mode: 2, trm: [0.15, 5, 'sine'] });
-    // synthesizer.play('A4', { dur: 1, mode: 2 });
-};
-
-document.getElementById('playNoise').onclick = () => {
-    // const waves = [new Wave(audioCtx, noiseWaveStrategy)];
-
-    // let periods = [
-    //     ...new Set(
-    //         [...Array(15)].flatMap((_, i) => Math.round(audioCtx.sampleRate / 441 / 1.5 ** i)),
-    //     ),
-    // ].filter(Boolean);
-    // // console.log(JSON.stringify(periods));
-    // const waves = [new Wave(audioCtx, noiseWaveStrategy, periods, Math.random)];
-
-    let FC_PERIODS = [4068, 2034, 1016, 762, 508, 380, 254, 202, 160, 128, 96, 64, 32, 16, 8, 4];
-    let periods = [
-        ...new Set(FC_PERIODS.map(fcp => Math.round((audioCtx.sampleRate / 1789772.5) * fcp))),
-    ].filter(Boolean);
-    console.log(periods);
-    const waves = [
-        new Wave(audioCtx, noiseWaveStrategy, periods, lfsr()),
-        new Wave(audioCtx, noiseWaveStrategy, periods, lfsr(6)),
-    ];
-
-    const synthesizer = new Synthesizer(audioCtx, waves);
-    // synthesizer.play(null);
-    // synthesizer.play('C4', { 'dur': 0.25, 'mode': 0, 'swp': 300 });
-    // synthesizer.play('A4', { dur: 1, mode: 2, vib: [20, 5, 'sawtooth'] });
-    // synthesizer.play('A4', { dur: 1, mode: 2, trm: [0.15, 5, 'sine'] });
-    // synthesizer.play('A4', { dur: 1, mode: 2 });
-    synthesizer.play(0, { dur: 0.5, mode: 0, trm: [0.15, 5, 'square'] });
-    // synthesizer.play(0, { dur: 0.5, mode: 0, trm: [0.15] });
-    // synthesizer.play(0, { dur: 0.5, mode: 0 });
-};
-
-document.querySelectorAll('#keyboard input').forEach(button => {
-    button.onclick = () => {
-        const waves = [new Wave(audioCtx, presetWaveStrategy, 'sawtooth')];
-
-        const noteNames = ['c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b'];
-        const noteName = button.value.toLowerCase();
-        const index = noteNames.indexOf(noteName);
-
-        const synthesizer = new Synthesizer(audioCtx, waves);
-        synthesizer.play(60 + index, { dur: 0.5 });
-    };
-});
 
 document.getElementById('resume').onclick = async () => {
     if (audioCtx.state === 'suspended') {
--- packages/audio/synthesizer/src/Synthesizer.mjs
+++ packages/audio/synthesizer/src/Synthesizer.mjs
@@ -1,3 +1,5 @@
+import { validatePlayArgs } from './validators/play.js';
+
 export class Synthesizer {
     #audioCtx;
     #waves;
@@ -22,9 +24,10 @@ export class Synthesizer {
         gain.setValueAtTime(volume * s, endTime - r);
         gain.linearRampToValueAtTime(0, endTime);
     }
-    play(
-        noteNumber,
-        {
+    play(noteNumber, options = {}) {
+        if (__DEV__) validatePlayArgs(noteNumber, options, this.#waves);
+
+        const {
             'mod': mode = 0,
             'sta': startTime = this.#audioCtx.currentTime,
             'dur': duration = 0.03,
@@ -32,56 +35,16 @@ export class Synthesizer {
             'det': detune = 0,
             'swp': sweep = 0,
             'env': envelope = [],
-            'vib': vibration,
+            'vib': vibrato,
             'trm': tremolo,
-        } = {},
-    ) {
-        if (__DEV__) {
-            if (Number.isInteger(mode) == false || mode < 0 || mode >= this.#waves.length)
-                throw new Error('Invalid mode');
-            if (Number.isNaN(startTime) || startTime < 0) throw new Error('Invalid start time');
-            if (Number.isNaN(duration) || duration <= 0) throw new Error('Invalid duration');
-            if (Number.isNaN(volume) || volume < 0 || volume > 2) throw new Error('Invalid volume');
-            if (Number.isNaN(detune) || detune < -3600 || detune > 3600)
-                throw new Error('Invalid detune');
-            if (Number.isNaN(sweep) || sweep < -3600 || sweep > 3600)
-                throw new Error('Invalid sweep');
-            if (Array.isArray(envelope) == false || envelope.length > 4)
-                throw new Error('Invalid envelope');
-            if (envelope[0] + envelope[1] + envelope[3] > duration)
-                throw new Error('Invalid envelope');
-            if (envelope[2] < 0 || envelope[2] > 1)
-                throw new Error('Invalid envelope sustain level');
-            envelope.forEach(value => {
-                if (Number.isNaN(value) || value < 0) throw new Error('Invalid envelope value');
-            });
-            if (vibration != undefined) {
-                if (Array.isArray(vibration) == false || vibration.length > 3)
-                    throw new Error('Invalid vibration');
-                let [depth, rate, wave] = vibration;
-                if (Number.isNaN(depth) || depth < 0 || depth > 100)
-                    throw new Error('Invalid vibration depth');
-                if (Number.isNaN(rate) || rate <= 0 || rate > 10)
-                    throw new Error('Invalid vibration rate');
-                if (typeof wave != 'string') throw new Error('Invalid vibration wave');
-            }
-            if (tremolo != undefined) {
-                if (Array.isArray(tremolo) == false || tremolo.length > 3)
-                    throw new Error('Invalid tremolo');
-                let [depth, rate, wave] = tremolo;
-                if (Number.isNaN(depth) || depth < 0 || depth > 1)
-                    throw new Error('Invalid tremolo depth');
-                if (Number.isNaN(rate) || rate <= 0 || rate > 10)
-                    throw new Error('Invalid tremolo rate');
-                if (typeof wave != 'string') throw new Error('Invalid tremolo wave');
-            }
-        }
+        } = options;
+
         let endTime = startTime + duration;
         let src = this.#waves[mode].getSourceNode(noteNumber);
         src.detune.value = detune;
         src.detune.linearRampToValueAtTime(detune + sweep, endTime);
-        if (src.frequency != undefined && vibration != undefined)
-            this.#lfo(src.frequency, ...vibration);
+        if (src.frequency != undefined && vibrato != undefined)
+            this.#lfo(src.frequency, ...vibrato);
         // if (!(this.#wav instanceof TableWave)) volume /= 4;
         let gainNode = new GainNode(this.#audioCtx);
         Synthesizer.envelope(gainNode.gain, startTime, endTime, volume, envelope);
