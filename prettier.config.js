1a85ca41a24c50dc187c274ff42ee7f3fe604364
m-nakasato <70959675+m-nakasato@users.noreply.github.com>
2026-07-26 11:46:30 +0900

    Remove arrowParens at prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -2,8 +2,6 @@ export default {
     singleQuote: true,
     // trailingComma: 'es5',
     tabWidth: 4,
-    // arrowParens: 'always',
-    arrowParens: 'avoid',
     quoteProps: 'preserve',
     printWidth: 100,
     htmlWhitespaceSensitivity: 'ignore',
