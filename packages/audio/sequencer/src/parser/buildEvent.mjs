7e8a8cb6f8ca214c9bca15695962cff43bc83091
m-nakasato <70959675+m-nakasato@users.noreply.github.com>
2026-08-04 08:12:27 +0900

    Refactor parser

--- packages/audio/sequencer/src/parser/buildEvent.mjs
+++ packages/audio/sequencer/src/parser/buildEvent.mjs
@@ -1,14 +1,7 @@
 import { duration } from './duration.mjs';
 
 export const buildEvent = (event, bpm, opt, defaultValue = '8') => {
-    let result;
-    const value = event[1] ? event[1] : defaultValue;
-    const dur = duration(value, bpm);
-    if (event[0] == '_') {
-        result = [event[0], { dur }];
-    } else {
-        const optNum = event[2] ? event[2] : 0;
-        result = [event[0] * 1, { dur, ...opt[optNum] }];
-    }
-    return result;
+    const dur = duration(event[1] ?? defaultValue, bpm);
+    if (event[0] == '_') return ['_', { dur }];
+    return [event[0] * 1, { dur, ...opt[event[2] ?? 0] }];
 };
